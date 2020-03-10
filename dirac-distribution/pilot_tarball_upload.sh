d5f2d830ba621104ebb17aa05dea17781dda32ef
Andre Sailer <andre.philippe.sailer@cern.ch>
2020-03-10 17:29:16 +0100

    pilot upload: add checksumming

--- dirac-distribution/pilot_tarball_upload.sh
+++ dirac-distribution/pilot_tarball_upload.sh
@@ -6,7 +6,12 @@ echo $tmpdir
 cp Pilot/*.py $tmpdir
 cp tests/pilot.json $tmpdir
 
-# create the tar and upload
+# create the tar
 cd $tmpdir
 tar -cf pilot.tar *.py
-( tar -cf - pilot.tar pilot.json ) | ssh uploaduser@lxplus.cern.ch "cd /eos/project/d/diracgrid/www/tars/Pilot/DIRAC/${1}/ && tar -xvf - "
+
+# make the checksums file
+sha512sum pilot.tar pilot.json *.py > checksums.sha512
+
+# upload all the files
+( tar -cf - pilot.tar pilot.json checksums.sha512 ) | ssh uploaduser@lxplus.cern.ch "cd /eos/project/d/diracgrid/www/tars/Pilot/DIRAC/${1}/ && tar -xvf - "
